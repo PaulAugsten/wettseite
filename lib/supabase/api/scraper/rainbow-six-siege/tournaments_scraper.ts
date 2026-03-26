f47a42719dcefcccdf1804bfd281425d801cca86
PaulAugsten <paulaugsten9@gmail.com>
2026-03-26 15:03:17 +0100

    finished tournaments scraper

--- lib/supabase/api/scraper/rainbow-six-siege/tournaments_scraper.ts
+++ lib/supabase/api/scraper/rainbow-six-siege/tournaments_scraper.ts
@@ -1,9 +1,7 @@
 import 'dotenv/config';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { Element } from 'domhandler';
 import { createClient } from '@supabase/supabase-js';
-import crypto from 'crypto';
 import { scrapePlayoffMatches } from './playoffmatches_scraper';
 
 const siegegg_tournaments_url = 'https://siege.gg/competitions?page=1&tier=1&type=Major';
@@ -17,22 +15,8 @@ type Tournament = {
     start_date: string;
     end_date: string;
     status: 'scheduled' | 'live' | 'finished';
-    url: string;
-};
-
-type Match = {
-    match_id: string;
-    tournament_id: number;
-    stage: string | null;
-    group: string | null;
-    bracket: string;
-    round: string;
-    team1_name: string;
-    team2_name: string;
-    team1_score: number | null;
-    team2_score: number | null;
-    status: 'scheduled' | 'live' | 'finished';
-    date: string;
+    lp_url: string;
+    sg_url: string;
 };
 
 async function getGameId() {
@@ -58,56 +42,6 @@ async function getGameId() {
         return 0;
     }
     return data.id;
-}
-
-function getStage(el: Element, $: cheerio.CheerioAPI) {
-    let current = $(el);
-
-    while (current.length) {
-        const previous = current.prev();
-
-        if (previous.is('h2, h3, h4')) {
-            return previous.text().trim();
-        }
-
-        current = current.parent();
-    }
-
-    return null;
-}
-
-function getGroup(el: Element, $: cheerio.CheerioAPI) {
-    let current = $(el);
-
-    while (current.length) {
-        const previous = current.prev();
-
-        if (previous.is('h2, h3, h4, h5')) {
-            const text = previous.text().trim();
-
-            const match = text.match(/Group\s+([A-Z])/i);
-            if (match) {
-                return match[1];
-            }
-        }
-
-        current = current.parent();
-    }
-
-    return null;
-}
-
-function getRound(el: Element, $: cheerio.CheerioAPI) {
-    return $(el).closest('.brkts-bracket').find('.brkts-header').first().text().trim();
-}
-
-function getMatchStatus(el: Element, $: cheerio.CheerioAPI): 'scheduled' | 'live' | 'finished' {
-    const hasScore = $(el).find('.match-info-header-scoreholder-score').length > 0;
-    const isFinished = $(el).find(".timer-object[data-finished='finished']").length > 0;
-
-    if (!hasScore) return 'scheduled';
-    if (isFinished) return 'finished';
-    return 'live';
 }
 
 function parseDateRange(input: string, year: number) {
@@ -166,29 +100,6 @@ function parseDateRange(input: string, year: number) {
     return { start_date, end_date };
 }
 
-function normalizeTeams(team1: string, team2: string) {
-    return [team1, team2].sort();
-}
-
-function generateMatchId(match: Match) {
-    const [team1_name_sorted, team2_name_sorted] = normalizeTeams(
-        match.team1_name,
-        match.team2_name,
-    );
-
-    const raw = [
-        match.tournament_id,
-        match.stage,
-        match.group,
-        match.round,
-        team1_name_sorted,
-        team2_name_sorted,
-        match.date,
-    ].join('|');
-
-    return crypto.createHash('md5').update(raw).digest('hex');
-}
-
 async function scrapeSiegeggTournamentPage(url: string) {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
@@ -243,7 +154,8 @@ async function scrapeSiegeggTournamentPage(url: string) {
             start_date: start_date_string,
             end_date: end_date_string,
             status,
-            url: tournament_url,
+            lp_url: '',
+            sg_url: tournament_url,
         });
     });
 
@@ -293,7 +205,8 @@ async function scrapeLiquipediaTournaments(url: string) {
             start_date: '',
             end_date: '',
             status: 'scheduled',
-            url: tournament_url,
+            lp_url: tournament_url,
+            sg_url: '',
         });
     });
 
@@ -303,39 +216,67 @@ async function scrapeLiquipediaTournaments(url: string) {
 async function scrapeTournaments(liquipedia_url: string, siegegg_url: string) {
     const liquipedia_tournaments = scrapeLiquipediaTournaments(liquipedia_url);
     const siegegg_tournaments = scrapeSiegeggTournaments(siegegg_url);
-    const tournaments: Tournament[] = [];
 
-    const liquipedia_names = new Set(
-        (await liquipedia_tournaments).map((t) =>
+    const liquipedia_map = new Map(
+        (await liquipedia_tournaments).map((t) => [
             t.name.replace('R6 ', '').split(' -')[0].split(' ').sort().join(' '),
-        ),
-    );
-    const siegegg_names = new Set(
-        (await siegegg_tournaments).map((t) => t.name.split(' ').sort().join(' ')),
+            t,
+        ]),
     );
 
-    const matches = [...liquipedia_names].filter((name) => siegegg_names.has(name));
+    const tournaments = (await siegegg_tournaments).map((sg_tournament) => {
+        const sorted_name = sg_tournament.name.split(' ').sort().join(' ');
+        const lp_tournament = liquipedia_map.get(sorted_name);
 
-    console.log(liquipedia_names);
-    console.log(siegegg_names);
-    console.log(matches);
-    console.log(matches.length);
+        if (
+            lp_tournament &&
+            lp_tournament.lp_url &&
+            sg_tournament.name &&
+            sg_tournament.game_id &&
+            sg_tournament.location &&
+            sg_tournament.start_date &&
+            sg_tournament.end_date &&
+            sg_tournament.status &&
+            sg_tournament.sg_url
+        ) {
+            return {
+                name: sg_tournament.name,
+                game_id: sg_tournament.game_id,
+                location: sg_tournament.location,
+                start_date: sg_tournament.start_date,
+                end_date: sg_tournament.end_date,
+                status: sg_tournament.status,
+                liquipedia_url: lp_tournament.lp_url,
+                siegegg_url: sg_tournament.sg_url,
+            };
+        }
 
-    (await siegegg_tournaments).forEach((tournament) => {
-        tournaments.push({
-            name: tournament.name,
-            game_id: tournament.game_id,
-            location: tournament.location,
-            start_date: tournament.start_date,
-            end_date: tournament.end_date,
-            status: tournament.status,
-            url: tournament.url,
-        });
+        return null;
     });
+
+    console.log(`Found ${tournaments.length} Tournaments.`);
+
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_SERVICE_ROLE_KEY!,
+        {
+            auth: {
+                autoRefreshToken: false,
+                persistSession: false,
+            },
+        },
+    );
+
+    const { data, error } = await supabase
+        .from('tournaments')
+        .upsert(tournaments, { onConflict: 'name' })
+        .select();
+
+    if (error || !data) {
+        console.log('Error inserting tournaments into the DB:', error);
+        return 0;
+    }
 }
 
-/*
-game_id = 2; Rainbow six siege
-*/
 // scrapeSiegeggTournaments(siegegg_tournaments_url).catch(console.error);
 scrapeTournaments(liquipedia_tournaments_url, siegegg_tournaments_url).catch(console.error);
