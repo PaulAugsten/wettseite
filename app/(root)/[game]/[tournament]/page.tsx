ce7af931c854950300e996088c6e4b6cf3dbc3b8
PaulAugsten <paulaugsten9@gmail.com>
2026-07-03 21:31:31 +0200

    fix: remove dead code and unused files

--- app/(root)/[game]/[tournament]/page.tsx
+++ app/(root)/[game]/[tournament]/page.tsx
@@ -194,30 +194,4 @@ export default async function Tournament({ params }: TournamentPageParameters) {
             </div>
         </div>
     );
-
-    /*
-    return (
-        <div>
-            <h1 className="text-6xl font-bold">{data.name}</h1>
-            <h2 className="text-4xl font-semibold">Upcoming Matches:</h2>
-
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols">
-                {(data?.matches as Match[]).map((match) => (
-                    <div
-                        key={match.id}
-                        className="bg-white shadow-md rounded-lg p-4 transition t..."
-                    >
-                        <h3 className="text-lg font-bold mb-2">{match.team1?.name}</h3>
-                        <h3 className="text-lg font-bold mb-2">{match.team2?.name}</h3>
-                        <p className="text-gray-600">Match ID: {match.id}</p>
-                        <p className="text-gray-500 text-sm">Date: {match.date}</p>
-                        <p className="text-gray-500 text-sm">Status: {match.status}</p>
-                        <Link target="_blank" href={`https://siege.gg/matches/${match.id}`}>
-                            Link
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );*/
 }
